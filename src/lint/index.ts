ce0b48f80d5aef4eacf8d4e100f10f4dac944254
zhangzhengsmiling <zhangzhengsmiling@163.com>
2021-11-17 23:34:37 +0800

    feat: utils, readAllFiles
    - 递归读取文件名称

--- src/lint/index.ts
+++ src/lint/index.ts
@@ -9,49 +9,46 @@ const isDirectory = (path: string) => {
     });
 }
 
-const list: string[] = [];
-
-const paths: string[] = []
-const readFiles = (entry: string) => {
-  paths.push(entry);
-  console.log(paths)
-  return promises.readdir(entry)
-    .then(ls => {
-      ls.forEach(async item => {
-        console.log(item, paths)
-        const fullPath = paths.join('/') + '/' + item;
-        const _isDir = await isDirectory(fullPath);
-        if (_isDir) {
-          readFiles(fullPath)
-        } else {
-          list.push(fullPath)
-        }
-      })
-      paths.pop()
-    })
+const isDirectorySync = (path: string) => {
+  const stats = fs.statSync(path);
+  return stats.isDirectory();
 }
+
+const list: string[] = [];
 
 const readAllFiles = (entry: string) => {
   const exist = fs.existsSync(entry);
   if (!exist) throw new Error('directory is not exists');
-  return isDirectory(entry)
-    .then(isDirectory => {
-      if (!isDirectory) throw new Error(`${entry} is not a directory`)
-    })
-    .then(() => {
-      console.log(`${entry} is a directory`);
-      return readFiles(entry)
-    })
+  const isDir = isDirectorySync(entry);
+  if (!isDir) throw new Error(`${entry} is not a directory`);
+  const paths: string[] = [];
+  const list: string[] = [];
+  const readFiles = (entry: string) => {
+    const ls = fs.readdirSync(entry)
+      ls.forEach(item => {
+        const fullPath = path.join(...paths, item)
+        const _isDir = isDirectorySync(fullPath);
+        if (_isDir) {
+          paths.push(item)
+          readFiles(fullPath)
+        } else {
+          const [, ...rest] = paths;
+          const _p = path.join(...rest, item);
+          list.push(_p)
+        }
+      })
+      paths.pop()
+  }
+  paths.push(entry);
+  readFiles(entry);
+  return list;
 }
 
 const lint = () => {
   // console.log('lint excute...')
   // 读取src下所有的文件
-  readAllFiles(path.resolve(process.cwd(), 'src'))
-    .then(() => {
-      console.log('list', list)
-    })
-
+  const files = readAllFiles(path.resolve(process.cwd(), 'src'))
+  console.log(files);
 }
 
 export default lint;
