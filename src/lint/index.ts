4d16b8b3d7d42c64ea1060164c2a78d104ffc6be
zhangzhengsmiling <zhangzhengsmiling@163.com>
2021-11-17 20:09:10 +0800

    feat: node异步编程测试

--- src/lint/index.ts
+++ src/lint/index.ts
@@ -1,23 +1,57 @@
 import fs, { promises } from 'fs';
 import path from 'path';
 
+const isDirectory = (path: string) => {
+  return promises.stat(path)
+    .then(stats => stats.isDirectory())
+    .catch(err => {
+      throw err;
+    });
+}
+
+const list: string[] = [];
+
+const paths: string[] = []
+const readFiles = (entry: string) => {
+  paths.push(entry);
+  console.log(paths)
+  return promises.readdir(entry)
+    .then(ls => {
+      ls.forEach(async item => {
+        console.log(item, paths)
+        const fullPath = paths.join('/') + '/' + item;
+        const _isDir = await isDirectory(fullPath);
+        if (_isDir) {
+          readFiles(fullPath)
+        } else {
+          list.push(fullPath)
+        }
+      })
+      paths.pop()
+    })
+}
+
 const readAllFiles = (entry: string) => {
   const exist = fs.existsSync(entry);
   if (!exist) throw new Error('directory is not exists');
-  promises.stat(entry)
-    .then(stats => {
-      // console.log(stats);
-
+  return isDirectory(entry)
+    .then(isDirectory => {
+      if (!isDirectory) throw new Error(`${entry} is not a directory`)
     })
-    .catch(err => {
-      throw err
+    .then(() => {
+      console.log(`${entry} is a directory`);
+      return readFiles(entry)
     })
 }
 
 const lint = () => {
   // console.log('lint excute...')
   // 读取src下所有的文件
-  readAllFiles(path.resolve(process.cwd(), 'aaa'))
+  readAllFiles(path.resolve(process.cwd(), 'src'))
+    .then(() => {
+      console.log('list', list)
+    })
+
 }
 
 export default lint;
