bf65ac2f3d711380f2a7441a9bdee56fd88e98ee
zhangzhengsmiling <zhangzhengsmiling@163.com>
2022-05-14 15:31:45 +0800

    chore: delete build results directory
    - 删除构建后的es文件夹

--- es/ci/common/json-formatter.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var jsonFormatter = function (space) {
-    return function (object) {
-        return JSON.stringify(object, null, space);
-    };
-};
-exports.default = jsonFormatter;
